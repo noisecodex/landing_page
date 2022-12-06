52c1541152765494860c9835aca7c2fce6b5c83f
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-12-05 23:53:17 -0500

    add: contnet into Biography component

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 const Biography = () => {
-  return <div>Biograhpy</div>;
+  return (
+    <Typography align="justify" variant="h6">
+      Mistic Roove se consolida como banda a finales del año 2019 en la cuidad
+      de Cuenca - Ecuador, luego de que un grupo de amigos influenciados por la
+      música de Jamaica empiezan a interpretar solo éxitos del reggae en español
+      e inglés. La banda tuvo muy buena acogida por el público en sus es
+      presentaciones en la escena local, luego de esto la banda se ve obligada a
+      hacer una para en las presentaciones en vivo con el fin de explorar y
+      buscar el sonido propio de la banda. Durante este tiempo algunos de los
+      integrantes deciden salir para dedicarse a sus proyectos personales
+      mientras se seguía trabajando en la composición de los temas. A finales
+      del año 2019 por esas casualidades de la vida llega el Naza a la banda con
+      el cual consolidan las ideas y empiezan a trabajar en la producción del
+      primer EP de la banda bajo el formato de power trío. Luego de grabar el EP
+      se une a la banda el Peter aportando con la trompeta los metales que le
+      faltaban a la banda y el Shanta en la batería y percusiones. Para nuestras
+      ultimas presentaciones contamos con músicos invitados con el cuales
+      compltamos la alineación completa de la banda. Mistic Roove tiene como
+      base musical sonidos del reggae y ska sobre el cual fusionan ritmos como
+      rock, punk, metal, jazz, ritmos latinos, entre otros ingredientes. Las
+      letras de nuestras canciones se basan en vivencias diarias como el amor,
+      odio, viajes, introspección, fiesta, protesta; música básicamente para
+      sentirla. Mistic Roove son: - Naza: Guitarra y voces - Shanta: Batería y
+      coros - Peter: Trompeta y coros - El Mishu: Bajo, coros y gritos
+    </Typography>
+  );
 };
 
 export default Biography;
