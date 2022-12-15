23036fe8db1ef71abbe83f02318ed9b57a896743
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-12-15 00:36:33 -0500

    add: logo image into Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Grid, Typography, Box, ImageList, ImageListItem } from "@mui/material";
 import mistic from "../assets/static/mistic.jpeg";
+import logo from "../assets/static/logo-blanco.png";
 
 const Home = () => {
   return (
     <>
-      <Grid item xs={6} sx={{ textAlign: "center" }}>
+      <Grid item xs={5} sx={{ textAlign: "center" }}>
+        <ImageList sx={{ width: 700, height: 203 }}>
+          <ImageListItem key="0001">
+            <img src={logo} alt="mistic roove" loading="lazy" />
+          </ImageListItem>
+        </ImageList>
+
         <Typography align="justify" variant="h6">
           Mistic Roove tiene como base musical sonidos del reggae y ska sobre el
           cual fusionan ritmos como rock, punk, metal, jazz, ritmos latinos,
@@ -14,8 +21,8 @@ const Home = () => {
           protesta; música básicamente para sentirla.
         </Typography>
       </Grid>
-      <Grid item xs={6} sx={{ textAlign: "center" }}>
-        <ImageList sx={{ width: 1500, height: 450 }} cols={3} rowHeight={164}>
+      <Grid item xs={7} sx={{ textAlign: "center" }}>
+        <ImageList sx={{ width: 1200, height: 300 }} rowHeigt={300}>
           <ImageListItem key="0001">
             <img src={mistic} alt="mistic roove" loading="lazy" />
           </ImageListItem>
